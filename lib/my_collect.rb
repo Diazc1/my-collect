d8a93064d0973e40f8b7cec11e2f51783e87c36c
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-09 00:06:11 -0800

    commit tree 5dd48ca9f4d149ff8518a4a92592d5f5d5e86d44

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+    i = 0
+    collection_one = []
+    while i < collection.length
+        collection_one << yield(collection[i])
+        i = i + 1
+    end
+    collection_one
+end
